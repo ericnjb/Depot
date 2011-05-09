2a2d4eebb4257dffebdc1f50f7479f45fb76535f
Eric Schrager <ericnjb@gmail.com>
2011-05-09 13:35:46 -0400

    Shopping Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
   
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   # validation stuff
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -9,4 +12,16 @@ class Product < ActiveRecord::Base
     :with   => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG impage.'
   }
+  
+  private
+  
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
---
+++ db/development_structure.sql
@@ -0,0 +1,5 @@
+INSERT INTO schema_migrations (version) VALUES ('20110506152752');
+
+INSERT INTO schema_migrations (version) VALUES ('20110509163941');
+
+INSERT INTO schema_migrations (version) VALUES ('20110509165105');
