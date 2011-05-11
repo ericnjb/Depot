3b9982ede019c6077e7cc23df19383ce7c8d5d82
Eric Schrager <ericnjb@gmail.com>
2011-05-11 15:31:13 -0400

    Language switcher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_i18n_locale_from_params
-  before_filter :authorize
+  before_filter :set_i18n_locale_from_params, :authorize
   protect_from_forgery
   
   private
